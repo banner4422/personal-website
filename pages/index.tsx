2ce5bfb68ffb1cb2e99f3bc690ee6ba55fc795d0
banner4422 <christiankroghnielsen@gmail.com>
2022-10-30 02:46:36 +0200

    fix: change name

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,11 +5,11 @@ const Home: NextPage = () => {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-2 bg-zinc-900">
       <Head>
-        <title>Christian Nielsen</title>
+        <title>Christian Krogh Nielsen</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <h1 className="text-6xl text-white">Christian Nielsen</h1>
+        <h1 className="text-6xl text-white">Christian Krogh Nielsen</h1>
         <br />
         <p className="text-white">Coming soon...™️</p>
       </main>
