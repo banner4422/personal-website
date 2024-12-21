f918c1d77d06287858b4e253ad4caa87fc3a8e3a
Christian Krogh Nielsen <christian@chrkn.dev>
2024-12-21 22:44:56 +0100

    fix: remove motion, just use framer-motion instead

--- src/components/AnimatedBars.tsx
+++ src/components/AnimatedBars.tsx
@@ -1,70 +1,47 @@
-"use client";
-import { animate } from 'motion';
-import { useEffect } from 'react';
+import { motion } from 'framer-motion';
 
 export default function AnimatedBars() {
-    useEffect(() => {
-      animate(
-        '#bar1',
-        {
-          transform: [
+    return (
+      <div className="w-auto flex items-end overflow-hidden">
+        <motion.span
+          animate={{ transform: [
             'scaleY(1.0) translateY(0rem)',
             'scaleY(1.5) translateY(-0.082rem)',
             'scaleY(1.0) translateY(0rem)'
-          ]
-        },
-        {
-          duration: 1.0,
-          repeat: Infinity,
-          easing: ['ease-in-out']
-        }
-      );
-      animate(
-        '#bar2',
-        {
-          transform: [
+          ]}}
+          transition={{
+            duration: 1.0,
+            repeat: Infinity,
+            easing: ['ease-in-out']
+          }}
+          className="w-1 mr-[3px] h-2 bg-gray-300 dark:bg-gray-500 opacity-75"
+        />
+        <motion.span
+          animate={{ transform: [
             'scaleY(1.0) translateY(0rem)',
             'scaleY(3) translateY(-0.083rem)',
             'scaleY(1.0) translateY(0rem)'
-          ]
-        },
-        {
-          delay: 0.2,
-          duration: 1.5,
-          repeat: Infinity,
-          easing: ['ease-in-out']
-        }
-      );
-      animate(
-        '#bar3',
-        {
-          transform: [
+          ]}}
+          transition={{
+            delay: 0.2,
+            duration: 1.5,
+            repeat: Infinity,
+            easing: ['ease-in-out']
+          }}
+          className="w-1 mr-[3px] h-1 bg-gray-300 dark:bg-gray-500"
+        />
+        <motion.span
+          animate={{ transform: [
             'scaleY(1.0)  translateY(0rem)',
             'scaleY(0.5) translateY(0.37rem)',
             'scaleY(1.0)  translateY(0rem)'
-          ]
-        },
-        {
-          delay: 0.3,
-          duration: 1.5,
-          repeat: Infinity,
-          easing: ['ease-in-out']
-        }
-      );
-    }, []);
-  
-    return (
-      <div className="w-auto flex items-end overflow-hidden">
-        <span
-          id="bar1"
-          className="w-1 mr-[3px] h-2 bg-gray-300 dark:bg-gray-500 opacity-75"
-        />
-        <span
-          id="bar2"
-          className="w-1 mr-[3px] h-1 bg-gray-300 dark:bg-gray-500"
-        />
-        <span
-          id="bar3"
+          ]}}
+          transition={{
+            delay: 0.3,
+            duration: 1.5,
+            repeat: Infinity,
+            easing: ['ease-in-out']
+          }}
           className="w-1 h-3 bg-gray-300 dark:bg-gray-500 opacity-80"
         />
       </div>
