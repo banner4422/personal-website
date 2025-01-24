b82c000c0350574b88f06c4576e2211580e8c3f7
Christian Krogh Nielsen <christian@chrkn.dev>
2025-01-24 22:58:13 +0100

    feat(app): add about page
    temporary, just for now

---
+++ src/app/about/page.tsx
@@ -0,0 +1,29 @@
+"use client";
+import Head from "next/head";
+import SocialMediaButton, {
+  socialMediaButtonsData,
+} from "../../components/SocialMediaButton";
+import { Fragment } from "react";
+import { AvatarCardWithDescription } from "../../components/AvatarCard";
+
+export default function Home() {
+  return (
+      <Fragment>
+        <Head>
+          <title>Christian Krogh Nielsen</title>
+        </Head>
+        <Fragment>
+          <div className="flex flex-col justify-center items-start max-w-2xl mx-auto">
+            <AvatarCardWithDescription />
+            <div
+              className="mx-auto md:flex-row flex flex-wrap text-center justify-center gap-4 md:gap-x-16 pb-4"
+            >
+              {socialMediaButtonsData.map((x) => (
+                <SocialMediaButton key={x.colour} colour={x.colour} link={x.link} />
+              ))} 
+            </div>
+          </div>
+        </Fragment>
+      </Fragment>
+  );
+};
