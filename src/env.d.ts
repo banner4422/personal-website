f985908f00c7bcfa94cc8638cd615e46ae33cf0c
Christian Krogh Nielsen <christian@chrkn.dev>
2026-03-16 17:12:45 +0100

    fix: use global declaration for Env interface

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,9 +1,13 @@
 /// <reference types="@cloudflare/workers-types" />
 
-interface Env {
-    SPOTIFY_CLIENT_ID: string;
-    SPOTIFY_CLIENT_SECRET: string;
-    SPOTIFY_REFRESH_TOKEN: string;
-    ASSETS: Fetcher;
-    SESSION: KVNamespace;
+declare global {
+    interface Env {
+        SPOTIFY_CLIENT_ID: string;
+        SPOTIFY_CLIENT_SECRET: string;
+        SPOTIFY_REFRESH_TOKEN: string;
+        ASSETS: Fetcher;
+        SESSION: KVNamespace;
+    }
 }
+
+export {};
