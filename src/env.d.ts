164353f589bc4ded34c011e2c3dcef1b855d532b
Christian Krogh Nielsen <christian@chrkn.dev>
2026-03-16 17:10:53 +0100

    fix: add type definitions for Cloudflare env vars

---
+++ src/env.d.ts
@@ -0,0 +1,9 @@
+/// <reference types="@cloudflare/workers-types" />
+
+interface Env {
+    SPOTIFY_CLIENT_ID: string;
+    SPOTIFY_CLIENT_SECRET: string;
+    SPOTIFY_REFRESH_TOKEN: string;
+    ASSETS: Fetcher;
+    SESSION: KVNamespace;
+}
